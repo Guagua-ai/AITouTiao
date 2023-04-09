b1083e19df2f2eae4de5c70bb0cf1f63ebcd0150
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-09 13:28:30 -0700

    Create a new env variable for deployment environment

--- search/index.py
+++ search/index.py
@@ -5,7 +5,7 @@ from algoliasearch.search_client import SearchClient
 def create_post_search_index():
     ALGOLIA_APP_ID = os.getenv('ALGOLIA_APP_ID')
     ALGOLIA_API_KEY = os.getenv('ALGOLIA_API_KEY')
-    ALGOLIA_INDEX_NAME = 'dev-post-index'
+    ALGOLIA_INDEX_NAME = os.getenv('DEPLOY_ENV') + '-post-index'
 
     # Initialize the Algolia client and index
     algolia_client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
@@ -16,7 +16,7 @@ def create_post_search_index():
 def create_user_search_index():
     ALGOLIA_APP_ID = os.getenv('ALGOLIA_APP_ID')
     ALGOLIA_API_KEY = os.getenv('ALGOLIA_API_KEY')
-    ALGOLIA_INDEX_NAME = 'dev-user-index'
+    ALGOLIA_INDEX_NAME = os.getenv('DEPLOY_ENV') + '-user-index'
 
     # Initialize the Algolia client and index
     algolia_client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
