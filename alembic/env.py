26d918b3ce00f188d9f51ff24ee58da7cba399f9
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-09 21:46:49 -0700

    Reinitialize alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
-from sqlalchemy import pool, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import pool
 
 from alembic import context
 
@@ -16,20 +14,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# create a new database engine
-engine = create_engine(config.get_main_option('sqlalchemy.url'))
-
-# bind the engine to the declarative base
-Base = declarative_base()
-Base.metadata.bind = engine
-
-# create a new session factory
-Session = sessionmaker(bind=engine)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
+# from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
 
 def run_migrations_offline() -> None:
@@ -44,11 +38,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=config.get_main_option('sqlalchemy.url'),
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={'paramstyle': 'named'},
+        dialect_opts={"paramstyle": "named"},
     )
 
     with context.begin_transaction():
@@ -69,11 +64,6 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        # bind the connection to the declarative base
-        Base.metadata.bind = connection
-        # create the tables if they don't exist
-        Base.metadata.create_all(connection)
-
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
