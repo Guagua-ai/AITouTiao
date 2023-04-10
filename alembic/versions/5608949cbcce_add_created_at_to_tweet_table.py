f75334df235a74e06a121077ddf0362f5335bf17
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-09 21:20:16 -0700

    Fix alembic script to add new column

--- alembic/versions/5608949cbcce_add_created_at_to_tweet_table.py
+++ alembic/versions/5608949cbcce_add_created_at_to_tweet_table.py
@@ -7,6 +7,7 @@ Create Date: 2023-04-08 20:15:38.328633
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,33 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('tweets', sa.Column('created_at', sa.DateTime))
+    op.drop_table('tweets')
 
 
 def downgrade() -> None:
-    pass
+    op.create_table('tweets',
+                    sa.Column('id', sa.INTEGER(),
+                              autoincrement=True, nullable=False),
+                    sa.Column('source_id', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('source_name', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('author', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('title', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('description', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('url', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('url_to_image', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('published_at', postgresql.TIMESTAMP(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('content', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.column('created_at', postgresql.TIMESTAMP(),
+                              autoincrement=False, nullable=True),
+                    sa.PrimaryKeyConstraint('id', name='tweets_pkey'),
+                    sa.UniqueConstraint('url', name='tweets_url_key')
+                    )
