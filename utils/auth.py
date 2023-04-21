d01b6a4f49a1e4c1fa04c7a5609282ad24756779
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-21 00:23:02 -0700

    Add user info validation

---
+++ utils/auth.py
@@ -0,0 +1,9 @@
+import re
+
+def is_valid_email(email):
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(email_regex, email))
+
+def is_valid_phone(phone):
+    phone_regex = r'^\+?[1-9]\d{1,14}$'
+    return bool(re.match(phone_regex, phone))
