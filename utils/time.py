2e3de0a15307df4594c2de9cdab9b00063a99858
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-20 22:44:00 -0700

    Convert time in Chinese

--- utils/time.py
+++ utils/time.py
@@ -1,9 +1,30 @@
-from datetime import datetime
+import locale
+from datetime import datetime, timedelta
 
 
-def standard_format(published_at: datetime.timestamp):
-    # Format the datetime object to the desired format
-    # published_at_formatted = published_at.strftime('%Y-%m-%d %H:%M:%S')
-    published_at_formatted = published_at.strftime('%a, %d %b %Y %H:%M:%S GMT')
+def standard_format(published_at: datetime):
+    # Set the locale to Chinese (Simplified)
+    try:
+        locale.setlocale(locale.LC_TIME, 'zh_CN.utf8')
+    except locale.Error:
+        try:
+            locale.setlocale(locale.LC_TIME, 'zh_CN.UTF-8')
+        except locale.Error:
+            raise Exception("Chinese locale not supported on your system.")
 
-    return published_at_formatted
+    # Calculate time difference
+    now = datetime.now()
+    time_diff = now - published_at
+    days = time_diff.days
+    hours = time_diff.seconds // 3600
+
+    if days == 0:
+        return f"{hours}小时前"
+    elif days == 1:
+        return "1天前"
+    elif days <= 7:
+        return f"{days}天前"
+    else:
+        # Format the datetime object to the desired format
+        published_at_formatted = published_at.strftime('%Y年%m月%d日')
+        return published_at_formatted
