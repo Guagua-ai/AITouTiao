449cad66b40db2332aca192dc98318f2418df1d4
Michael Zhou <michael.zhouzzy@gmail.com>
2023-04-23 20:48:46 -0700

    Run formatter

--- run.py
+++ run.py
@@ -10,14 +10,14 @@ def create_app():
     create_database_if_not_exists('news_dev')
     with app.app_context():
         db.create_all()
-        
+
     return app
+
 
 if __name__ == '__main__':
     # Run the CLI
     cli_runner = CliRunner(os.getenv('OPENAI_API_KEY'))
     cli_runner.run()
-
 
     # Create the database tables
     create_database_if_not_exists('news_dev')
